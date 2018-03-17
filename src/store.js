50b681a8eb0ceb3372d8cec68e0f0e2b92a79438
Norbert Kaźmierczak <norbini12@gmail.com>
2018-03-17 16:38:21 +0100

    Change files structure and split auth process to more files

--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from "redux"
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-import reducer from "./reducers"
-
-export default createStore(reducer, {}, applyMiddleware(logger, thunk))
