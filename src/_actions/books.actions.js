e4f71bb12d5912579aba14887c062e4c19da7361
msciborski <michal.sciborski@student.put.poznan.pl>
2018-09-26 21:00:11 +0200

    Create books recommendation action

--- src/_actions/books.actions.js
+++ src/_actions/books.actions.js
@@ -2,6 +2,7 @@ import { booksConstants } from '../_constants';
 import { booksService } from '../_services';
 import { alertActions } from './';
 import { userActions } from './';
+import { request } from 'http';
 
 export const booksActions = {
     getUserBooks,
@@ -11,7 +12,8 @@ export const booksActions = {
     editBook,
     deleteBook,
     sendBook,
-    setArgs
+    setArgs,
+    getBookRecommendations,
 };
 
 
@@ -28,7 +30,7 @@ function getUserBooks(args) {
                 error => {
                     dispatch(failure(error.message));
                     dispatch(alertActions.error(error.message, error.status));
-                    
+
                 }
             );
     };
@@ -36,6 +38,26 @@ function getUserBooks(args) {
     function request() { return { type: booksConstants.GET_BOOKS_REQUEST } }
     function success(books, args) { return { type: booksConstants.GET_BOOKS_SUCCESS, books, args } }
     function failure(error) { return { type: booksConstants.GET_BOOKS_FAILURE, error } }
+}
+
+function getBookRecommendations(bookId) {
+  return dispatch => {
+    dispatch(request());
+
+    return booksService.getBookRecommendations(bookId)
+      .then(recommendations => {
+        dispatch(success(recommendations))
+        },
+        error => {
+          dispatch(failure(error.message));
+          dispatch(alertActions.error(error.message, error.status));
+        }
+      );
+  };
+
+  function request() { return { type: booksConstants.GET_BOOK_RECOMMENDATIONS_REQUEST } }
+  function success(recommendations) { return { type: booksConstants.GET_BOOK_RECOMMENDATIONS_SUCCESS, recommendations } }
+  function failure(error) { return { type: booksConstants.GET_BOOK_RECOMMENDATIONS_FAILURE, error } }
 }
 
 
@@ -61,7 +83,7 @@ function getBook(bookId) {
                 error => {
                     dispatch(failure(error.message));
                     dispatch(alertActions.error(error.message, error.status));
-                    
+
                 }
             );
     };
@@ -88,7 +110,7 @@ function deleteBook(bookId) {
                 error => {
                     dispatch(failure(error.message));
                     dispatch(alertActions.error(error.message, error.status));
-                    
+
                 }
             );
     };
@@ -159,7 +181,7 @@ function uploadBook(files) {
 
         return booksService.uploadBook(files)
             .then(
-                files => { 
+                files => {
                     dispatch(this.getUserBooks()); // refresh books list
                     dispatch(success(files));
                     dispatch(alertActions.success("Uploaded!"));
@@ -183,7 +205,7 @@ function editBook(bookId, data) {
 
         return booksService.editBook(bookId, data)
             .then(
-                book => { 
+                book => {
                     dispatch(this.getUserBooks()); // refresh books list
                     dispatch(this.getBook(bookId)); // refresh book
                     dispatch(success());
