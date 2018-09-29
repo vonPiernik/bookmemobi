3deb592876baac0bc5682d3a12dc993cb749878b
msciborski <michal.sciborski@student.put.poznan.pl>
2018-09-29 17:36:35 +0200

    Span with text if book dosent have cover

--- src/Dashboard/BookRecommendationsModal/BookRecommendation.jsx
+++ src/Dashboard/BookRecommendationsModal/BookRecommendation.jsx
@@ -4,7 +4,17 @@ export default function BookRecommendation(props) {
   const { title, coverUrl, author } = props.book;
   return (
     <div className="book-recommendation">
-      <div className="book-cover"><img className="book-cover" src={coverUrl} alt={title}/></div>
+
+      <div className="book-cover">
+      {
+        coverUrl &&
+          <img className="book-cover" src={coverUrl} alt={title}/>
+      }
+      {
+        !coverUrl &&
+          <span>This book has no cover.</span>
+      }
+      </div>
       <p className="book-recommendation-author">{author}</p>
       <p className="book-recommendation-title">{title}</p>
     </div>
